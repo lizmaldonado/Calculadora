3a4b100f1823267d7d0f4d8d8ecb93e678d2de18
lizmaldonado <lizmaldonado@live.com.mx>
2018-07-15 14:51:29 -0500

    se agregan nuevas imagenes como el on y se implementan

--- js/app.js
+++ js/app.js
@@ -1,111 +1,127 @@
   
-  var cuatro = document.getElementById('4');
-  var cinco = document.getElementById('5');
-  var seis = document.getElementById('6');
-  var punto=document.getElementById('punto');
-  var mas=document.getElementById('mas');
-  var entre=document.getElementById('dividido');
-  var por=document.getElementById('por');
-  var menos=document.getElementById('menos');
-  var sign=document.getElementById('sign');
-  var igual=document.getElementById('igual')
-  var cifra1=0;
-  var bandera=0, decimal=0;
-  var resultado="";
-  var operador="mas"; signo="+";
-  var operador1=0;  //si o esta obteniendo la cifra1, si operador1=1 obtener cifra2
-  var contador=0,num1,num2;
+ var cifra1=0,  decimal=0, contador=0;
+var resultado="";
+var operador="mas"; signo="+";
+var operador1=0;  //si o esta obteniendo la cifra1, si operador1=1 obtener cifra2
+var CeroIzq="false";
+var num1,num2;
 
+var on=document.getElementById('on');
+var sign=document.getElementById('sign');
 
-igual.addEventListener("mousedown",function(x) {igual.style = "padding:10px";}),
-mas.addEventListener("mousedown",function(x) {mas.style = "padding:10px";}),
+var entre=document.getElementById('dividido');
+
+var por=document.getElementById('por');
+var cuatro = document.getElementById('4');
+var cinco = document.getElementById('5');
+var seis = document.getElementById('6');
+
+var menos=document.getElementById('menos');
+
+var cero=document.getElementById('0');
+var punto=document.getElementById('punto');
+var igual=document.getElementById('igual');
+
+var mas=document.getElementById('mas');
+
+on.addEventListener("mousedown",function(x) {on.style = "padding:10px";}),
+sign.addEventListener("mousedown",function(x) {sign.style = "padding:10px";}),
 entre.addEventListener("mousedown",function(x) {entre.style = "padding:10px";}),
-sign.addEventListener("mousedown",function(x) {signo.style = "padding:10px";}),
 por.addEventListener("mousedown",function(x) {por.style = "padding:10px";}),
-menos.addEventListener("mousedown",function(x) {menos.style = "padding:10px";}),
-punto.addEventListener("mousedown",function(x) {punto.style = "padding:10px";}),
 cuatro.addEventListener("mousedown",function(x) {cuatro.style = "padding:10px";}),
 cinco.addEventListener("mousedown",function(x) {cinco.style = "padding:10px";}),
-seis.addEventListener("mousedown",function(x) {seis.style = "padding:10px";})
+seis.addEventListener("mousedown",function(x) {seis.style = "padding:10px";}),
+menos.addEventListener("mousedown",function(x) {menos.style = "padding:10px";}),
+cero.addEventListener("mousedown",function(x) {cero.style = "padding:10px";}),
+punto.addEventListener("mousedown",function(x) {punto.style = "padding:10px";}),
+igual.addEventListener("mousedown",function(x) {igual.style = "padding:10px";}),
+mas.addEventListener("mousedown",function(x) {mas.style = "padding:10px";}),
 
-  igual.addEventListener("mouseup",function(x)
+on.addEventListener("mouseup",function(x)
     {
-    igual=document.getElementById('igual').getAttribute("id")
-      igual.style = "padding:0px";
-      cifra2=resultado;
-      bandera=1;
-      decimal=0; //ya puede volver a escribir un "."
-      signo="+";
-      resolver(cifra1,cifra2);
-    }),
-  mas.addEventListener("mouseup",function(x)
-    {
-      mas=document.getElementById('mas').getAttribute("id")
-      mas.style = "padding:0px";
-      cifra1=resultado;
+      on.style = "padding:0px";
+      cifra1=0;
       resultado="";
-      operador="mas"
-      operador1=1;
+      operador="mas";
+      decimal=0;
+      operador1=0;
+      num1=0,num2=0;
+      CeroIzq=false;
       limpiardisplay();
     }),
 
-    entre.addEventListener("mouseup",function(x)
+igual.addEventListener("mouseup",function(x)
+    {
+      igual.style = "padding:0px";
+      cifra2=resultado;
+      if (cifra2=="")
       {
-        entre=document.getElementById('dividido').getAttribute("id")
+        cifra2=cifra1;
+      }
+      decimal=0; //ya puede volver a escribir un "."
+      signo="+";
+      resolver(cifra1,cifra2);
+      contador=0;
+    }),
+
+mas.addEventListener("mouseup",function(x)
+    {
+      mas.style = "padding:0px";
+      operador=document.getElementById('mas').getAttribute("id");
+      PrepLim();
+    }),
+
+entre.addEventListener("mouseup",function(x)
+      {
         entre.style = "padding:0px";
-        cifra1=resultado;
-        resultado="";
         operador="entre"
-        operador1=1;
-        limpiardisplay();
+        PrepLim();
       }),
 
-      por.addEventListener("mouseup",function(x)
+  por.addEventListener("mouseup",function(x)
         {
-          por=document.getElementById('por').getAttribute("id")
           por.style = "padding:0px";
-          cifra1=resultado;
-          resultado="";
-          operador="por"
-          operador1=1;
-          limpiardisplay();
+          operador=document.getElementById('por').getAttribute("id");
+          PrepLim();
         }),
 
-        menos.addEventListener("mouseup",function(x)
+  menos.addEventListener("mouseup",function(x)
           {
-            menos=document.getElementById('menos').getAttribute("id")
             menos.style = "padding:0px";
-            cifra1=resultado;
-            resultado="";
-            operador="menos"
-            operador1=1;
-            limpiardisplay();
+            operador=document.getElementById('menos').getAttribute("id")
+            PrepLim();
           }),
 
-    punto.addEventListener("mouseup",function(x)
+  punto.addEventListener("mouseup",function(x)
       {
-        x=document.getElementById('punto').getAttribute("id")
-        x=".";
+        //x=document.getElementById('punto').getAttribute("id")
         punto.style = "padding:0px";
+        x=".";
         if (contador<8)
           { if (decimal==0)
-            { resultado=resultado+x;
+            { if (contador==0)  //Punto al principio
+                {
+                  resultado="0"+resultado;
+                  contador=contador+1
+                }
+              resultado=resultado+x;
               contador=contador+1;
               decimal=1; //me indica que ya tiene un punto
-              imprime(x)
+              imprime(resultado)
             }
           }
       }),
-      sign.addEventListener("mouseup",function(x)
-        { var cadena=""
-          x=document.getElementById('sign').getAttribute("id")
+
+  sign.addEventListener("mouseup",function(x)
+        {
+          var cadena=""
           sign.style = "padding:0px";
           if (signo=="+")
               {
                 x="-";
                 resultado=x+resultado;
                 contador=contador+1;
-                imprime(x)
+                imprime(resultado)
               }
           else
            {
@@ -120,14 +136,9 @@ seis.addEventListener("mousedown",function(x) {seis.style = "padding:10px";})
 
   cuatro.addEventListener("mouseup",function(x)
     {
-      x=document.getElementById('4').getAttribute("id")
       cuatro.style = "padding:0px";
-      if (contador<8)
-        {
-          resultado=resultado+x;
-          contador=contador+1;
-          imprime(x)
-        }
+      x=document.getElementById('4').getAttribute("id")
+      CDisplay(x);
     }),
 
     cinco.addEventListener("mouseup",function(x)
@@ -138,26 +149,35 @@ seis.addEventListener("mousedown",function(x) {seis.style = "padding:10px";})
         {
           resultado=resultado+x;
           contador=contador+1;
-          imprime(x)
+          imprime(resultado)
         }
       }),
       seis.addEventListener("mouseup",function(x)
         {
-          x=document.getElementById('6').getAttribute("id")
           seis.style = "padding:0px";
+          x=document.getElementById('6').getAttribute("id")
+
           if (contador<8)
           {
             resultado=resultado+x;
             contador=contador+1;
-            imprime(x)
+            imprime(resultado)
           }
-        })
+        }),
+    cero.addEventListener("mouseup",function(x)
+      {
+        cero.style = "padding:0px";
+        x=document.getElementById('0').getAttribute("id");
+         if (contador==0)  //cero al principio
+         {
+          CeroIzq="true";
+         }
+           CDisplay(x);
+      })
 
 
-function imprime(x)
-  {
-    document.getElementById("display").innerHTML = resultado;
-  }
+
+
 
 function limpiardisplay()
   {
@@ -167,7 +187,7 @@ function limpiardisplay()
     contador=0;
     decimal=0;
     signo="+";
-  }
+  }  //termina funcion limpiardisplay
 
 function resolver()
   {
@@ -188,27 +208,74 @@ function resolver()
   	        break;
 
        case "entre":
-  	       res = (num1) / (num2);
-  	        break;
+            if (num2!==0)
+            {
+              res = (num1) / (num2);
+            }
+            else
+              {res="-ERROR-"}
+            break;
+
   	}
+    cifra1=res;   //esto es para el caso de que usuario no meta cifra1
     truncar(res)
 
     imprime(resultado)
+    CeroIzq="false"
     resultado="";
-	}
+	}  //termina funcion resolver
+
 function CdnaInt()
   {
     num1=parseFloat(cifra1)
     num2=parseFloat(cifra2)
   }
+
 function truncar(res)
   {
     var res1 ="", cortado="";
     res1=String(res);
     if (res1.length>8)
       {
-      cortado=res1.substring(0,8);
-      res=parseFloat(cortado);
+        cortado=res1.substring(0,8);
+        res=parseFloat(cortado);
       }
     resultado=res;
+  } //termina funcion truncar
+
+function imprime(resultado)
+    {
+      var temp=0;
+      if (CeroIzq=="true" && contador==2)
+      {
+      temp=resultado.substring(1);
+      contador=contador-1;
+      resultado=temp;
+      }
+      document.getElementById("display").innerHTML = resultado;
+      if (temp!==0 && contador==1)
+         {
+           CeroIzq="false"
+         }
+    }
+
+function CDisplay(x)
+{
+  if (contador<8)
+    {
+      resultado=resultado+x;
+      contador=contador+1;
+      imprime(resultado)
+    }
+} //termina funcion CDisplay
+function PrepLim()
+{
+  if (resultado=="")  //usuario no dio cifra1
+   {
+    resultado=cifra1;
   }
+  cifra1=resultado;
+  resultado="";
+  operador1=1;
+  limpiardisplay();
+}
